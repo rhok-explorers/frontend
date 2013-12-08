db656b72ba1bb072e280c551b7a93f3e77fad6a5
djhell <daniele@brugnara.me>
2013-12-08 09:37:47 +0100

    fix path ng ed aggiunti files routes.html e .js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,19 @@
 'use strict';
 
 angular.module('ngApp', [
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+        'ngRoute'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/routes', {
+                templateUrl: 'views/routes.html',
+                controller: 'RoutesCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('ngApp')
-    .controller('MainCtrl', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+    .controller('RoutesCtrl', function ($scope) {
+        //
     });
